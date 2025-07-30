8a83f52357230536227335a05b5dd24f8f5b7cb0
WinTux <rusok.software@gmail.com>
2025-07-30 18:11:02 -0400

    Primer commit

---
+++ src/main/java/com/pepe/proyectospringtool/ProyectoSpringToolApplication.java
@@ -0,0 +1,13 @@
+package com.pepe.proyectospringtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pepe/proyectospringtool/ProyectoSpringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepe.proyectospringtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
