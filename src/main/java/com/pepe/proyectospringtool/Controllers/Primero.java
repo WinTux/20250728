46c3da50dec268f87c191af6b7a697f632b026dd
WinTux <rusok.software@gmail.com>
2025-07-30 20:02:13 -0400

    Perfiles y debug

--- src/main/java/com/pepe/proyectospringtool/Controllers/Primero.java
+++ src/main/java/com/pepe/proyectospringtool/Controllers/Primero.java
@@ -1,14 +1,27 @@
 package com.pepe.proyectospringtool.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Primero {
+	
+	@Autowired
+	private ApplicationContext contexto;
+	
 	@RequestMapping("/")
 	@ResponseBody
 	public String saludo() {
 		return "Buenas noches :D";
+	}
+	
+	@RequestMapping("/cerrar")
+	@ResponseBody
+	public void cerrar() {
+		SpringApplication.exit(contexto);
 	}
 }
