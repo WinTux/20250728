d3f6a95a816b49bd9f1dfd6a86ea0dbb869a45c7
WinTux <rusok.software@gmail.com>
2025-08-01 19:39:50 -0400

    Logging

--- src/main/java/com/pepe/proyectospringtool/Controllers/HomeController.java
+++ src/main/java/com/pepe/proyectospringtool/Controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.pepe.proyectospringtool.Controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value="/home") // localhost:7001/home
 public class HomeController {
+	private static final Logger logger = Logger.getLogger(HomeController.class.getName());
+	
 	//@GetMapping // localhost:7001/home [GET]
 	@RequestMapping(method=RequestMethod.GET)
 	public String index() {
+		logger.warning("Es posible que el recurso no se muestre correctamente.");
 		return "home/index";
 	}
 }
--- src/main/java/com/pepe/proyectospringtool/Controllers/Primero.java
+++ src/main/java/com/pepe/proyectospringtool/Controllers/Primero.java
@@ -1,5 +1,7 @@
 package com.pepe.proyectospringtool.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,15 +15,19 @@ public class Primero {
 	@Autowired
 	private ApplicationContext contexto;
 	
+	private static final Logger logger = LoggerFactory.getLogger(Primero.class);
+	
 	@RequestMapping("/")
 	@ResponseBody
 	public String saludo() {
+		logger.info("Se alcanzó al método saludo() retornando un saludo.");
 		return "Buenas noches :D";
 	}
 	
 	@RequestMapping("/cerrar")
 	@ResponseBody
 	public void cerrar() {
+		logger.info("Se alcanzó el método cerrar() por lo que el cliente no recibe respuesta alguna.");
 		SpringApplication.exit(contexto);
 	}
 }
