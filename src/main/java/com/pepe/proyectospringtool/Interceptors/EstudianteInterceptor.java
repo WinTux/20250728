4f53884f00b277c86e8abdbaf677220024a97bae
WinTux <rusok.software@gmail.com>
2025-08-11 19:58:56 -0400

    Interceptores 1

---
+++ src/main/java/com/pepe/proyectospringtool/Interceptors/EstudianteInterceptor.java
@@ -0,0 +1,20 @@
+package com.pepe.proyectospringtool.Interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class EstudianteInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		return true;
+	}
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		
+	}
+}
