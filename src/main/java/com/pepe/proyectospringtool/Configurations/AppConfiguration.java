123ac06add1b91ca8e8a81c74090dac9c17b80f8
WinTux <rusok.software@gmail.com>
2025-08-15 20:08:33 -0400

    https

--- src/main/java/com/pepe/proyectospringtool/Configurations/AppConfiguration.java
+++ src/main/java/com/pepe/proyectospringtool/Configurations/AppConfiguration.java
@@ -1,11 +1,14 @@
 package com.pepe.proyectospringtool.Configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.pepe.proyectospringtool.Filter.ControlConexionFilter;
 
@@ -24,5 +27,14 @@ public class AppConfiguration {
 		fil.addUrlPatterns("/estudiante/*");
 		fil.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return fil;
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		return http
+				.securityMatcher(EndpointRequest.toAnyEndpoint())
+				.authorizeHttpRequests(authorize -> 
+					authorize.anyRequest().permitAll()
+				).build();
 	}
 }
